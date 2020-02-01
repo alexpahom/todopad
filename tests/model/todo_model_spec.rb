a1150e604f938b8f46a1626b92f427fc82cd2d8f
Alex Pahomchik <alex.pahomchik@gmail.com>
2020-02-01 21:26:06 +0300

    more tests

--- tests/model/todo_model_spec.rb
+++ tests/model/todo_model_spec.rb
@@ -6,35 +6,69 @@ class TodoModelTest < BaseCase
   include Minitest::Assertions
 
   describe 'Task should be working' do
+
+    def err
+      @err = @task.errors.messages
+    end
+
+    def rand
+      @rand = Time.now.strftime('%d%H%M%S%L').to_i
+    end
+
     it 'Title should present' do
-      task = Task.create(rank: 1, status: :open)
-      assert task.invalid?, 'Task title cannot be empty!'
+      @task = Task.create(rank: rand, status: :open)
+      assert @task.invalid?, "Task title cannot be empty!. #{err}"
     end
 
     it 'title should not be long' do
-      task = Task.create(title: 'a' * 35, rank: 1, status: :open)
-      assert task.invalid?, 'Task title cannot be longer 30 signs!'
+      @task = Task.create(title: 'a' * 35, rank: rand, status: :open)
+      assert @task.invalid?, "Task title cannot be longer 30 signs!. #{err}"
     end
 
     it 'description can present' do
-      task = Task.create(title: 'case', description: 'hello', rank: 2, status: :progress)
-      binding.pry
-      assert task.valid?, 'Task should be able to have description'
+      @task = Task.create(title: 'case', description: 'hello', rank: rand, status: :progress)
+      assert @task.valid?, "Task should be able to have description. #{err}"
     end
 
     it 'description can be empty' do
-      task = Task.create(title: 'case', description: nil, rank: 2, status: :progress)
-      assert task.valid?, 'Task should be able not to have description'
+      @task = Task.create(title: 'case', description: nil, rank: rand, status: :progress)
+      assert @task.valid?, "Task should be able not to have description. #{err}"
+    end
+
+    it 'status should present' do
+      @task = Task.create(title: 'Test', rank: rand)
+      assert @task.invalid?, "Task must have status. #{err}"
+    end
+
+    it 'status should match appropriate value' do
+      %i(open progress close).each do |status|
+        @task = Task.create(title: "Allah case", rank: rand, status: status)
+        assert @task.valid?, "Task of #{status} status should be valid. #{err}"
+      end
+    end
+    
+    it 'status cannot be arbitrary' do
+      @task = Task.create(title: "Allah case", rank: rand, status: :trololo)
+      assert @task.invalid?, "Task of :trololo status should be invalid. #{err}"
+    end
+
+    it 'rank should present' do
+      @task = Task.create(title: "Allah case", status: :open)
+      assert @task.invalid?, "Task cannot be without rank. #{err}"
+    end
+
+    it 'rank should be integer' do
+      @task = Task.create(title: "Allah case", status: :open, rank: 1.40)
+      assert @task.invalid?, "Task cannot save float rank. #{err}"
+    end
+
+    it 'rank should be unique for certain status' do
+      rank = rand
+      @task1 = Task.create(title: "Regular task", status: :close, rank: rank)
+      @task = Task.create(title: "Regular task2", status: :close, rank: rank)
+      assert @task.invalid?, "Should not save task with existing rank. #{err}"
     end
   end
-  #
-  #describe 'status should present'
-  #
-  #describe 'status should match appropriae value'
-  #
-  #describe 'rank should present'
-  #
-  #describe 'rank should be integer'
-  #
-  #describe 'rank should be unique for certain status'
+
+
 end
--- todo_model.rb
+++ todo_model.rb
@@ -7,7 +7,7 @@ class Task
   field :title, type: String
   field :description, type: String
   field :rank, type: Integer
-  field :status, type: String
+  field :status, type: Symbol
 
   validates :title, presence: true, length: { maximum: 30 }
   validates :rank, presence: true, numericality: { only_integer: true }, uniqueness: {
